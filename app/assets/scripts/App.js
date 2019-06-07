9afde4b6fb2a4c9505547320470710426da61696
Naif <hello@builtbynaif.com>
2019-06-07 12:56:44 +0530

    first draft

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -7,12 +7,13 @@ import Modal from './modules/Modal';
 import HeroSlider from './modules/HeroSlider';
 import Responsive from './modules/Responsive';
 import SlideOnScroll from './modules/SlideOnScroll';
+import SlideOnScrollLeft from './modules/SlideOnScrollLeft';
 
 var mobileMenu = new MobileMenu();
 var stickyheader = new StickyHeader();
 var modal = new Modal();
 var heroSlider = new HeroSlider(); 
 var responsive = new Responsive(); 
-new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%"); 
-new SlideOnScroll($(".slideIn"), "75%");
+new RevealOnScroll($(".reveal-item"), "85%");
+new SlideOnScroll($(".slideIn"), "75%"); 
+new SlideOnScrollLeft($(".slideInLeft"), "75%");
--- app/assets/scripts/modules/HeroSlider.js
+++ app/assets/scripts/modules/HeroSlider.js
@@ -2,18 +2,22 @@ import $ from 'jquery';
 import slick from 'slick-carousel';
 
 class HeroSlider {
-  constructor() {
-    this.els = $(".hero-slider");
-    this.initSlider();
-  }
+    constructor() {
+        this.els = $(".hero-slider");
+        this.initSlider();
+    }
 
-  initSlider() {
-    this.els.slick({
-      autoplay: true,
-      arrows: false,
-      dots: true
-    });
-  }
+    initSlider() {
+        this.els.slick({
+            autoplay: true,
+            arrows: false,
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+    }
 }
 
 export default HeroSlider;
